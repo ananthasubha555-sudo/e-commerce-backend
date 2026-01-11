330b85ff281b52f601fc3d1dce0374d96644cf44
ananthasubha555-sudo <ananthasubha555@gmail.com>
2026-01-11 17:03:33 +0530

    Import product controller functions in server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import axios from 'axios';
 import mongoose from "mongoose";
+import { getProducts, getProductById, getCategories } from './controllers/productController.js';
 
 
 const app = express();
